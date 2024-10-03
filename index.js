8782f84c60f3c03bc625cfa15ae03cda839aff5d
Victor Ezaki <victorezaki19@gmail.com>
2024-10-03 18:07:14 -0300

    feat: Mais exercícios na arquitetura MVC

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const router = require('./src/routes/exercicio');
 const express = require('express');
 const app = express();
 
-app.use(router)
 app.use(express.json())
+app.use(router)
 
 app.listen(3000, () => {
     console.log("Servidor está rodando na porta 3000")
--- src/routes/exercicio.js
+++ src/routes/exercicio.js
@@ -5,150 +5,28 @@ const router = express.Router();
 
 router.post("/exercicio1", ControllerExercicio.Ex01);
 
-// router.post("/exercicio2", (req, res) => {
-//     // http://localhost:3000/exercicio2?valorHora=9.8&horas=800
-//     try {
-//         const { valorHora, horas } = req.body
-
-//         const result = funcoes.ex02(valorHora, horas);
-
-//         res.status(200).json({ result: result })
-//     } catch (error) {
-//         res.status(422).json({ msg: error.message })
-//     }
-// })
+router.post("/exercicio2", ControllerExercicio.Ex02);
 
 router.post("/exercicio3", ControllerExercicio.Ex03);
 
-// router.post("/exercicio4", (req, res) => {
-//     try {
-//         const { temp } = req.body
-//         const result = funcoes.ex04(temp)
-//         res.status(200).json({ result: result })
-//     } catch (error) {
-//         res.status(422).json({ msg: error.message })
-//     }
-// })
+router.post("/exercicio4", ControllerExercicio.Ex04);
 
-// router.post("/exercicio5", (req, res) => {
-//     try {
-//         const { milhas } = req.body
-//         const result = funcoes.ex05(milhas)
-//         res.status(200).send({ result: result })
-//     } catch (error) {
-//         res.status(422).json({ msg: error.message })
-//     }
-// })
+router.post("/exercicio5", ControllerExercicio.Ex05);
 
-// router.post("/exercicio6", (req, res) => {
-//     try {
-//         const { sec } = req.body
+router.post("/exercicio6", ControllerExercicio.Ex06);
 
-//         const results = funcoes.ex06(sec);
+router.post("/exercicio7", ControllerExercicio.Ex07);
 
-//         res.status(200).send({ results })
-//     } catch (error) {
-//         res.status(422).send({ msg: error.message })
-//     }
-// })
+router.post("/exercicio8", ControllerExercicio.Ex08);
 
-// router.post("/exercicio7", (req, res) => {
-//     const { km } = req.body
+router.post("/desafio01", ControllerExercicio.Desafio01);
 
-//     let array = funcoes.ex07(km);
+router.post("/desafio02", ControllerExercicio.Desafio02);
 
-//     res.send({
-//         results: {
-//             metros: array[0],
-//             centimetro: array[1]
-//         }
-//     })
-// })
+router.post("/desafio03", ControllerExercicio.Desafio03);
 
-// router.post("/exercicio8", (req, res) => {
-//     try {
-//         const { num } = req.body
-        
-//         let tabuada = funcoes.ex08(num);
-        
-//         res.status(200).send({
-//             results: {
-//                 x0: tabuada[0],
-//                 x1: tabuada[1],
-//                 x2: tabuada[2],
-//                 x3: tabuada[3],
-//                 x4: tabuada[4],
-//                 x5: tabuada[5],
-//                 x6: tabuada[6],
-//                 x7: tabuada[7],
-//                 x8: tabuada[8],
-//                 x9: tabuada[9],
-//                 x10: tabuada[10]
-//             }
-//         })
-//     } catch (e) {
-//         res.status(422).json({ msg: e.message })
-//     }
-// })
+router.post("/exercicio9", ControllerExercicio.Ex09);
 
-// router.post("/desafio01", (req, res) => {
-//     const { idade } = req.body;
-
-//     let array = funcoes.desafio01(idade);
-
-//     res.send({
-//         results: {
-//             idade: idade,
-//             meses: array[0],
-//             dias: array[1]
-//         }
-//     })
-// })
-
-// router.post("/desafio02", (req, res) => {
-//     const { a, b } = req.body;
-
-//     let antes = [a, b];
-
-//     let depois = funcoes.desafio02(a, b);
-
-//     res.send({
-//         results: {
-//             antes: {
-//                 a: antes[0],
-//                 b: antes[1]
-//             },
-//             depois: {
-//                 a: depois[0],
-//                 b: depois[1],
-//             }
-//         }
-//     })
-// })
-
-// router.post("/desafio03", (req, res) => {
-//     const { num1, num2 } = req.body;
-
-//     const maior = funcoes.desafio03(num1, num2);
-
-//     res.send({
-//         results: {
-//             maior: maior
-//         }
-//     })
-// })
-
-// router.post("/exercicio9", (req, res) => {
-//     const { nota1, nota2, nota3 } = req.body;
-
-//     const aluno = funcoes.ex09(nota1, nota2, nota3);
-
-//     res.send({
-//         results: {
-//             media: aluno.media,
-//             situacao: aluno.situacao
-//         }
-//     })
-// })
+router.post("/exercicio14", ControllerExercicio.Ex13);
 
 module.exports = router;
